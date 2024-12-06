LeafOS-Project/android_frameworks_base

SystemUI: Check primitive vibration support in new quick affordances
 * and provide an alternative if not

[SamarV-121: Use HEAVY_CLICK effect on activation and deactivation]

Change-Id: I9b6fafd731c49bf4d875b0ad581ee1b4666daf17

packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardBottomAreaViewBinder.kt
@@ -383,6 +383,12 @@ object KeyguardBottomAreaViewBinder {
         private val longPressDurationMs = ViewConfiguration.getLongPressTimeout().toLong()
         private var longPressAnimator: ViewPropertyAnimator? = null
 
+        private val areAllPrimitivesSupported = vibratorHelper?.areAllPrimitivesSupported(
+            VibrationEffect.Composition.PRIMITIVE_TICK,
+            VibrationEffect.Composition.PRIMITIVE_QUICK_RISE,
+            VibrationEffect.Composition.PRIMITIVE_QUICK_FALL
+        ) ?: false
+
         @SuppressLint("ClickableViewAccessibility")
         override fun onTouch(v: View?, event: MotionEvent?): Boolean {
             return when (event?.actionMasked) {
@@ -470,7 +476,12 @@ object KeyguardBottomAreaViewBinder {
                                             CycleInterpolator(ShakeAnimationCycles)
                                         shakeAnimator.start()
 
-                                        vibratorHelper?.vibrate(Vibrations.Shake)
+                                        vibratorHelper?.vibrate(
+                                            if (areAllPrimitivesSupported) {
+                                                Vibrations.Shake
+                                            } else {
+                                                Vibrations.ShakeAlt
+                                            })
                                     }
                                 } else {
                                     null
@@ -493,9 +504,17 @@ object KeyguardBottomAreaViewBinder {
             view.setOnClickListener {
                 vibratorHelper?.vibrate(
                     if (viewModel.isActivated) {
-                        Vibrations.Activated
+                        if (areAllPrimitivesSupported) {
+                            Vibrations.Activated
+                        } else {
+                            Vibrations.ActivatedAlt
+                        }
                     } else {
-                        Vibrations.Deactivated
+                        if (areAllPrimitivesSupported) {
+                            Vibrations.Deactivated
+                        } else {
+                            Vibrations.DeactivatedAlt
+                        }
                     }
                 )
                 viewModel.onClicked(
@@ -623,6 +642,7 @@ object KeyguardBottomAreaViewBinder {
                     }
                 }
                 .compose()
+        val ShakeAlt = VibrationEffect.createPredefined(VibrationEffect.EFFECT_DOUBLE_CLICK)
 
         val Activated =
             VibrationEffect.startComposition()
@@ -637,6 +657,7 @@ object KeyguardBottomAreaViewBinder {
                     0,
                 )
                 .compose()
+        val ActivatedAlt = VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK)
 
         val Deactivated =
             VibrationEffect.startComposition()
@@ -651,5 +672,6 @@ object KeyguardBottomAreaViewBinder {
                     0,
                 )
                 .compose()
+        val DeactivatedAlt = VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK)
     }
 }

halcyonproject/frameworks_base-mirror

Signed-off-by: HeroBuxx <me@herobuxx.me>

packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardBottomAreaViewBinder.kt
@@ -276,7 +276,12 @@ object KeyguardBottomAreaViewBinder {
                             isVisible ->
                             settingsMenu.animateVisibility(visible = isVisible)
                             if (isVisible) {
-                                vibratorHelper?.vibrate(KeyguardBottomAreaVibrations.Activated)
+                                vibratorHelper?.vibrate(
+                                    if (KeyguardBottomAreaVibrations.areAllPrimitivesSupported) {
+                                        KeyguardBottomAreaVibrations.Activated
+                                    } else {
+                                        KeyguardBottomAreaVibrations.ActivatedAlt
+                                    })
                                 settingsMenu.setOnTouchListener(
                                     KeyguardSettingsButtonOnTouchListener(
                                         viewModel = viewModel.settingsMenuViewModel,
@@ -443,7 +448,12 @@ object KeyguardBottomAreaViewBinder {
                     shakeAnimator.doOnEnd { view.translationX = 0f }
                     shakeAnimator.start()
 
-                    vibratorHelper?.vibrate(KeyguardBottomAreaVibrations.Shake)
+                    vibratorHelper?.vibrate(
+                        if (KeyguardBottomAreaVibrations.areAllPrimitivesSupported) {
+                            KeyguardBottomAreaVibrations.Shake
+                        } else {
+                            KeyguardBottomAreaVibrations.ShakeAlt
+                        })
                 }
                 view.onLongClickListener =
                     OnLongClickListener(falsingManager, viewModel, vibratorHelper, onTouchListener)
@@ -513,9 +523,17 @@ object KeyguardBottomAreaViewBinder {
                 )
                 vibratorHelper?.vibrate(
                     if (viewModel.isActivated) {
-                        KeyguardBottomAreaVibrations.Activated
+                        if (KeyguardBottomAreaVibrations.areAllPrimitivesSupported) {
+                            KeyguardBottomAreaVibrations.Activated
+                        } else {
+                            KeyguardBottomAreaVibrations.ActivatedAlt
+                        }
                     } else {
-                        KeyguardBottomAreaVibrations.Deactivated
+                        if (KeyguardBottomAreaVibrations.areAllPrimitivesSupported) {
+                            KeyguardBottomAreaVibrations.Deactivated
+                        } else {
+                            KeyguardBottomAreaVibrations.DeactivatedAlt
+                        }
                     }
                 )
             }